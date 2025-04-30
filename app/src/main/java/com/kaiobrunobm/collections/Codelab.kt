9967ca2228d675b29d39f5d5eee25f6a6aeacf10
kaiobrunobm <bmkaiobruno@gmail.com>
2025-04-30 15:57:38 -0300

    Learned about lists, sets and maps mutable and imutable

--- app/src/main/java/com/kaiobrunobm/collections/Codelab.kt
+++ app/src/main/java/com/kaiobrunobm/collections/Codelab.kt
@@ -1,2 +1,9 @@
 package com.kaiobrunobm.collections
 
+fun main() {
+    val solarSystem = listOf<String>(
+        "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"
+    )
+
+    println(solarSystem.size)
+}
